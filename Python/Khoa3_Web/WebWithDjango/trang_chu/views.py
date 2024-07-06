33f43b47cb40cb216744639847e757ec26008bd8
Harito97 <harito9703@gmail.com>
2024-07-06 21:45:33 +0700

    chore: Update trang_chu view to render web_covid.html

--- Python/Khoa3_Web/WebWithDjango/trang_chu/views.py
+++ Python/Khoa3_Web/WebWithDjango/trang_chu/views.py
@@ -4,5 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     ...
-    return HttpResponse("Hello, world. You're at the trang chu index.")
+    # return HttpResponse("Hello, world. You're at the trang chu index.")
+    return render(request, 'web_covid.html')
 

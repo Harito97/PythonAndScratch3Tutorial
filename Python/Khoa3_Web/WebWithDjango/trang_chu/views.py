d63128bbb950b680ec505e049c9d44ebd12567d8
Harito97 <harito9703@gmail.com>
2024-07-20 19:59:27 +0700

    chore: Refactor ChatBot code for improved user experience

--- Python/Khoa3_Web/WebWithDjango/trang_chu/views.py
+++ Python/Khoa3_Web/WebWithDjango/trang_chu/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     ...
     # return HttpResponse("Hello, world. You're at the trang chu index.")
-    return render(request, 'web_covid.html')
-
+    # return render(request, 'web_covid.html')
+    return HttpResponse(html_page)

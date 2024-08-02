f80242a6cccb130868a7647a34a949febfa7eba5
Harito97 <harito9703@gmail.com>
2024-08-03 00:18:06 +0700

    chore: Add API endpoint for index page

--- Python/Khoa3_Web/api/views.py
+++ Python/Khoa3_Web/api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    a = 3
+    http_response = '' 
+    content = f'''Hello, world. You're at the api index.<br>int a = {a}'''
+    http_response += content
+    content = ...
+    http_response += content
+    return HttpResponse(http_response)
